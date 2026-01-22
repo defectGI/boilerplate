a9aa95c2119ff2ab9e613cdc9df58eeb12ed9afb
defectGI <durupunarkaan@gmail.com>
2026-01-22 17:55:44 +0300

    refresh tokeni database'e bağlıcan

--- main.py
+++ main.py
@@ -58,6 +58,12 @@ def create_access_token(data: dict):
     expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, private_key, algorithm=ALGORITHM)
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, private_key, algorithm=ALGORITHM)
+
 
 @app.post("/AUTH/LOGIN")
 async def login(email, password):
