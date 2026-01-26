6b2aa8e0038b7d4049fe3347cd7106f31a62849a
defectGI <durupunarkaan@gmail.com>
2026-01-26 16:12:01 +0300

    removed data folder from tracking

--- datalocal/login_attempts.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE login_attempts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    ip_address TEXT NOT NULL,
-    email TEXT,
-    attempt_count INTEGER DEFAULT 1,
-    first_attempt DATETIME DEFAULT CURRENT_TIMESTAMP,
-    last_attempt DATETIME DEFAULT CURRENT_TIMESTAMP,
-    locked_until DATETIME
-);
